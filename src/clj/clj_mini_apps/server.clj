49b7a318eaf159774606fe32198fe6b366f7e13c
Pancia <anthony.dayzerostudio@gmail.com>
2015-01-10 14:29:09 -0800

    Addendum to previous commit.

---
+++ src/clj/clj_mini_apps/server.clj
@@ -0,0 +1,24 @@
+(ns clj-mini-apps.server
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+            [ring.util.response :as response]
+            [hiccup.core :refer :all]
+            [hiccup.def :refer [defhtml]]
+            [compojure.core :refer :all]
+
+            [clj-mini-apps.splash :refer :all]
+            [clj-mini-apps.tic-tac-toe :refer :all]))
+
+(defroutes main-routes
+  (GET "/" []
+       (splash))
+
+  (GET "/games/tic-tac-toe"  []
+       (tic-tac-toe))
+
+  (route/resources "/")
+
+  (route/not-found "ERROR 404, NOT FOUND"))
+
+(def app
+  (handler/site main-routes))
