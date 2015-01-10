ca1d80fd9f08f9e3d42e14d18b01a79fa836827a
wc_karatekid <rtanase@ucsc.edu>
2015-01-10 15:28:14 -0800

    Got canvas showing

--- src/clj/clj_mini_apps/tic_tac_toe.clj
+++ src/clj/clj_mini_apps/tic_tac_toe.clj
@@ -3,12 +3,17 @@
 
 (defhtml tic-tac-toe []
   [:head
-   [:title "Tic Tac Toe"]]
+   [:title {:style "color:white; text-align:center"} "Tic Tac Toe"]]
 
   [:body
-   [:h1 "Tic Tac Toe"]
-   [:canvas {:id "canvas"}]
+   [:h1 {:style "text-align:center"}"Tic Tac Toe"]
+   [:canvas {:id "canvas"
+             :width 600
+             :height 600
+             :style "margin-left:auto; 
+                     margin-right:auto;
+                     display:block"}]
    [:script {:src "http://code.jquery.com/jquery-2.1.3.min.js"}]
    [:script {:src "/js/cljs.js"}]
-   [:script "clj_mini_apps.tic-tac-toe.init();"]
+   [:script "clj_mini_apps.tic_tac_toe.init();"]
   ])
--- src/cljs/clj_mini_apps/tic-tac-toe.cljs
+++ src/cljs/clj_mini_apps/tic-tac-toe.cljs
@@ -1,14 +1,24 @@
 (ns clj-mini-apps.tic-tac-toe
-  (:require [monet.canvas :as canvas])
+  (:require [monet.canvas :as canvas]
+            [jayq.util :refer [log]]))
 
 (def canvas-dom (.getElementById js/document "canvas"))
 
 (def monet-canvas (canvas/init canvas-dom "2d"))
 
-(canvas/add-entity monet-canvas :background
-  (canvas/entity {:x 0 :y 0 :w 600 :h 600}
-                  nil
+(defn ^:export init []
+  (if (and js/document (.-getElementById js/document))
+    (do (log "ttt init")
+        (canvas/add-entity monet-canvas :background
+          (canvas/entity {:x 0 :y 0 :w 600 :h 600}
                   (fn [ctx val]
                     (-> ctx
-                      (canvas/fill-style "#191d21")
-                      (canvas/fill-rect val))))))
+                      (canvas/circle {:x 100 :y 100 :r 100})))
+                  (fn [ctx val]
+                    (-> ctx
+                      (canvas/fill-style "#d1d1d2")
+                      (canvas/fill-rect val)
+                      (canvas/fill-style "#FFFFFF")
+                      (canvas/fill-rect {:x 9 :y 9 :w 582 :h 582})
+                      (canvas/stroke-style "#d1d1d2")
+                      (canvas/circle {:x 100 :y 100 :r 100}))))))))
