50406e5e5a4c6745ac821d7dbf7fc52133ea6763
Pancia <anthony.dayzerostudio@gmail.com>
2015-01-10 14:45:04 -0800

    Added scripts to TTT

--- src/clj/clj_mini_apps/tic_tac_toe.clj
+++ src/clj/clj_mini_apps/tic_tac_toe.clj
@@ -6,4 +6,7 @@
    [:title "Tic Tac Toe"]]
 
   [:body
-   [:p "I'm the body, for ttt"]])
+   [:p "I'm the body, for ttt"]
+   [:script {:src "http://code.jquery.com/jquery-2.1.3.min.js"}]
+   [:script {:src "/js/cljs.js"}]
+   [:script "clj_mini_apps.tic-tac-toe.init();"]])
