7f25fc3006364b35dcb37afb2d18252808da5edd
wc_karatekid <rtanase@ucsc.edu>
2015-01-10 18:12:57 -0800

    Changed squares to O's

--- src/cljs/clj_mini_apps/tic-tac-toe.cljs
+++ src/cljs/clj_mini_apps/tic-tac-toe.cljs
@@ -8,7 +8,7 @@
   (when (and js/document (.-getElementById js/document))
     (do (log "ttt init"))))
 
-(defn setup []
+(defn init-board []
   ;Create background and outline
   (q/background 209 209 210)
   (q/fill 255)
@@ -20,27 +20,38 @@
   (q/rect 370 79 9 441)
   (q/rect 79 220 441 9)
   (q/rect 79 370 441 9)
-  (q/rect-mode :center)
-  {:rects []})
+  (q/rect-mode :center))
 
-(defn draw 
-  [{:keys [:rects]}]
-  (q/fill 255 0 0)
-  (doseq [{:keys [x y w h]} rects]
-    (q/rect x y w h))
-  {:keys [x y w h]})
+(def board
+  [[nil nil nil]
+   [nil nil nil]
+   [nil nil nil]])
 
-(defn add-rect
-  [state event]
+(def borders
+  (map #(assoc % :w 141 :h 141)
+    [{:top-left {:x 0 :y 0}}
+     {:top-mid {:x 229 :y 0}}
+     {:top-right {:x 379 :y 0}}
+     {:mid-left {:x 0 :y 229}}
+     {:mid-mid {:x 229 :y 229}}
+     {:mid-right {:x 379 :y 229}}
+     {:btm-left {:x 0 :y 379}}
+     {:btm-mid {:x 229 :y 379}}
+     {:btm-right {:x 379 :y 379}}]))
+
+(defn add-circle
+  []
   (log (q/mouse-x))
   (log (q/mouse-y))
   (q/fill 255 0 0)
-  (q/rect (q/mouse-x) (q/mouse-y) 10 10))
+  (q/ellipse (q/mouse-x) (q/mouse-y) 100 100)
+  (q/fill 255)
+  (q/ellipse (q/mouse-x) (q/mouse-y) 90 90))
   ;(update-in state [:rects]
-   ;          conj {:x (q/mouse-x) :y (q/mouse-y) :w 10 :h 10}))
+  ;          conj {:x (q/mouse-x) :y (q/mouse-y) :w 10 :h 10}))
 
 (q/defsketch hello
   :host "canvas"
   :size [600 600]
-  :setup setup
-  :mouse-pressed add-rect)
+  :setup init-board
+  :mouse-pressed add-circle)
