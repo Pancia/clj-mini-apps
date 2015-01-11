3f30c58f2a1b51689b87befcb122984a6a06977a
wc_karatekid <rtanase@ucsc.edu>
2015-01-10 17:32:36 -0800

    Still not working...

--- src/cljs/clj_mini_apps/tic-tac-toe.cljs
+++ src/cljs/clj_mini_apps/tic-tac-toe.cljs
@@ -8,7 +8,7 @@
   (when (and js/document (.-getElementById js/document))
     (do (log "ttt init"))))
 
-(defn draw []
+(defn setup []
   ;Create background and outline
   (q/background 209 209 210)
   (q/fill 255)
@@ -19,9 +19,25 @@
   (q/rect 220 79 9 441)
   (q/rect 370 79 9 441)
   (q/rect 79 220 441 9)
-  (q/rect 79 370 441 9))
+  (q/rect 79 370 441 9)
+  (q/rect-mode CENTER)
+
+(defn draw 
+  [{:keys [:rects]}]
+  (q/fill 255 0 0)
+  (doseq [{:keys [x y w h]} rects]
+    (q/rect x y w h)))
+
+(defn add-rect
+  [state {:keys [x y]}]
+  (log state)
+  (log mouse-x)
+  (update-in state [:rects]
+             conj {:x mouse-x :y mouse-y :w 10 :h 10}))
 
 (q/defsketch hello
+  :setup setup
   :draw draw
   :host "canvas"
-  :size [600 600])
+  :size [600 600]
+  :mouse-pressed add-rect)
