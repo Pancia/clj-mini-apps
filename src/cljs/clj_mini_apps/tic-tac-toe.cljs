952ed193e21fc31974e45ca61e4f1677977bd3c2
Pancia <anthony.dayzerostudio@gmail.com>
2015-01-10 22:14:44 -0800

    TTT works! TODO: make functional w/ m/fun-mode

--- src/cljs/clj_mini_apps/tic-tac-toe.cljs
+++ src/cljs/clj_mini_apps/tic-tac-toe.cljs
@@ -23,22 +23,26 @@
   (q/rect 79 370 441 9)
   (q/rect-mode :center))
 
+(defn new-board []
+  [[nil nil nil]
+   [nil nil nil]
+   [nil nil nil]])
+
 (def board
   (atom
-    [[nil nil nil]
-     [nil nil nil]
-     [nil nil nil]]))
+    (new-board)))
 
 (def cells
   (for [x [79 229 379]
         y [79 229 379]
-        :let [[bx by] [(+ x 70)
+        :let [[cx cy] [(+ x 70)
                        (+ y 70)]
-              [row col] [(dec (quot bx 141))
-                         (dec (quot by 141))]]]
+              [row col] [(dec (quot cx 141))
+                         (dec (quot cy 141))]]]
     {:x x :y y
      :w 141 :h 141
-     :center [bx by] :tile [row col]}))
+     :center [cx cy]
+     :tile [row col]}))
 
 (defn is-in-cell?
   [{:keys [x y w h]} [mx my]]
@@ -53,23 +57,94 @@
        (filter #(is-in-cell? % [mx my]))
        (first)))
 
-(defn add-circle
-  [mx my]
+(defn add-circle!
+  [[mx my] [tx ty]]
   (let [[x y] (:center (mouse->cell [mx my]))]
+    (swap! board #(assoc-in % [tx ty] :o))
     (when (every? identity [x y])
       (q/fill (q/random 255) (q/random 255) (q/random 255))
       (q/ellipse x y 100 100)
       (q/fill 255)
       (q/ellipse x y 90 90))))
 
-(defn play-ttt []
+(defn tile->center
+  [[tx ty]]
+  (->> cells
+       (map (fn [{:keys [center tile]}]
+              (when (= [tx ty] tile)
+                center)))
+       (filter identity)
+       (first)))
+
+(defn cpu-move! []
+  (let [get-rand-tile (fn [] [(rand-int 3) (rand-int 3)])]
+    (loop [[tx ty] (get-rand-tile)]
+      (if (not (get-in @board [tx ty]))
+        (let [[cpu-x cpu-y] (tile->center [tx ty])]
+          (do (swap! board #(assoc-in % [tx ty] :x))
+              (q/fill (q/random 255) (q/random 255) (q/random 255))
+              (q/ellipse cpu-x cpu-y 70 70)))
+        (recur (get-rand-tile))))))
+
+(defn winner?! [board]
+  (letfn [(check-winner [rc]
+            (when (apply = rc)
+              (first rc)))]
+    (or
+      (check-winner (nth board 0))
+      (check-winner (nth board 1))
+      (check-winner (nth board 2))
+      (check-winner (map first board))
+      (check-winner (map second board))
+      (check-winner (map #(nth % 2) board))
+      (check-winner [(get-in board [0 0]) (get-in board [1 1]) (get-in board [2 2])])
+      (check-winner [(get-in board [0 2]) (get-in board [1 1]) (get-in board [2 0])]))))
+
+(defn clear-canvas! []
+  (q/rect-mode :corner)
+  (q/background 209 209 210)
+  (q/fill 255)
+  (q/rect 9 9 582 582)
+  (q/fill 0)
+  (q/rect 220 79 9 441)
+  (q/rect 370 79 9 441)
+  (q/rect 79 220 441 9)
+  (q/rect 79 370 441 9)
+  (q/rect-mode :center))
+
+(defn check-winner! []
+  (if-let [winner ({:o "You" :x "The CPU"} (winner?! @board))]
+    (do (log (str winner " Won!"))
+        (log "Starting A New Game")
+        (reset! board (new-board))
+        (clear-canvas!))
+    (when (->> (flatten @board)
+               (filter nil?)
+               count zero?)
+      (do (log "It was a tie!")
+          (log "Starting A New Game")
+          (reset! board (new-board))
+          (clear-canvas!)))))
+
+(defn move-player
+  [[mx my] [tx ty]]
+  (do (add-circle! [mx my] [tx ty])
+      (check-winner!)))
+
+(defn move-cpu []
+  (do (cpu-move!)
+      (check-winner!)))
+
+(defn play-ttt! []
   (let [[mx my] [(q/mouse-x) (q/mouse-y)]
         [tx ty] (:tile (mouse->cell [mx my]))]
-    (if true
-      (add-circle mx my))))
+    (if (and (every? identity [tx ty])
+             (not (get-in @board [tx ty])))
+      (do (move-player [mx my] [tx ty])
+          (move-cpu)))))
 
 (q/defsketch hello
   :host "canvas"
   :size [600 600]
   :setup init-board
-  :mouse-pressed play-ttt)
+  :mouse-pressed play-ttt!)
