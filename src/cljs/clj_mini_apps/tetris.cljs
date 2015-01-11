01805811da37070f418784b2c7b3d0a17184e59c
wc_karatekid <rtanase@ucsc.edu>
2015-01-11 04:30:07 -0800

    Stopped paddles from going over borders

---
+++ src/cljs/clj_mini_apps/tetris.cljs
@@ -0,0 +1,5 @@
+(ns clj-mini-apps.tetris
+  (:require [quil.core :as q :include-macros true]
+            [quil.middleware :as m]
+            [jayq.core :as jq :refer [$]]
+            [jayq.util :refer [log]]))
