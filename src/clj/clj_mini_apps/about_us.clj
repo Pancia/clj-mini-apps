e0b248bc5f0006bde7a0462a0a05b502b4a82146
wc_karatekid <rtanase@ucsc.edu>
2015-03-25 11:19:58 -0700

    Added an about us page

---
+++ src/clj/clj_mini_apps/about_us.clj
@@ -0,0 +1,29 @@
+(ns clj-mini-apps.about-us
+  (:require [hiccup.def :refer [defhtml]]))
+
+(defhtml about-us []
+	[:head
+		[:meta {:http-equiv "Content-type"
+           :content "text/html; charset=utf-8"}]
+    	[:title "About Us"]
+    	[:link {:rel "shortcut icon" :href "/images/game controller logo small.png"}]
+    	[:link {:rel "stylesheet" :href "/css/bootstrap.min.css"}]
+   		[:link {:rel "stylesheet" :href "/css/splash.css"}]]
+	[:body
+		[:div
+		  [:ul {:class "nav nav-pills"}
+		    [:li {:role "presentation"}
+		      [:a {:href "/"} "Home"]]
+		    [:li {:role "presentation"}
+		      [:a {:href ""} "Games"]]
+		    [:li {:role "presentation"}
+		      [:a {:href "/blog"} "Blog"]]
+		    [:li {:role "presentation"}
+		      [:a {:href "/gallery"} "Gallery"]]
+		    [:li {:role "presentation"
+				  :class "active"}
+		      [:a {:href "/about-us"} "About Us"]]]]
+		[:p "Welcome to the About Us Page!"]
+		[:script {:src "http://code.jquery.com/jquery-2.1.3.min.js"}]
+   		[:script {:src "/js/bootstrap.min.js"}]
+    	[:script {:src "/js/cljs.js"}]])
---
+++ src/cljs/clj_mini_apps/about_us.cljs
@@ -0,0 +1,3 @@
+(ns clj-mini-apps.about-us
+	(:require [jayq.core :as jq :refer [$]]
+            [jayq.util :refer [log]]))
