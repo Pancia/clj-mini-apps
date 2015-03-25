1da23ea037c103aa790ab1ddfc2c68da79a2cba8
wc_karatekid <rtanase@ucsc.edu>
2015-03-25 11:20:11 -0700

    Created cljs files for the gallery and blog

---
+++ src/cljs/clj_mini_apps/blog.cljs
@@ -0,0 +1,4 @@
+(ns clj-mini-apps.blog
+	(:require [jayq.core :as jq :refer [$]]
+            [jayq.util :refer [log]]))
+
---
+++ src/cljs/clj_mini_apps/gallery.cljs
@@ -0,0 +1,3 @@
+(ns clj-mini-apps.gallery
+	(:require [jayq.core :as jq :refer [$]]
+            [jayq.util :refer [log]]))
